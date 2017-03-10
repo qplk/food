f8617976b0ae12d1a7c7369f3af275a15271ce87
qplk <Nail.khairullin@yandex.ru>
2017-03-10 16:33:54 +0300

    Update WebSecurityConfig.java

--- src/main/java/com/registration/reg/WebSecurityConfig.java
+++ src/main/java/com/registration/reg/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
-                    .loginPage("/welcome")
+                    .loginPage("/welcome/login")
                     .defaultSuccessUrl("/welcome")
                     .permitAll()
                     .and()
