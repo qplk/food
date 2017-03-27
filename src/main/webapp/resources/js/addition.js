79cf243be8477707f0f0739a698d9ef82233e42b
qplk <Nail.khairullin@yandex.ru>
2017-03-27 21:42:15 +0300

    Update addition.js

--- src/main/webapp/resources/js/addition.js
+++ src/main/webapp/resources/js/addition.js
@@ -1,13 +1,28 @@
-$("#plus").click(function add(){
-     var val = $("#res1").val();
-     val++;
-     $("#res1").val(val);
+$.ajax({
+type: 'POST',
+url: 'https://www.google.com/recaptcha/api/siteverify',
+
 })
-$("#minus").click(function remove(){
-    var val = $("#res1").val();
+
+function add(i){
+     var val = $("#res" + i).val();
+     val++;
+     $("#res" + i).val(val);
+}
+
+function remove(i){
+    var val = $("#res" + i).val();
     val--;
     if (val < 0){
-    $("#res1").val(0);
-    }else{$("#res1").val(val);
+    $("#res" + i).val(0);
+    }else{$("#res" + i).val(val);
+    }
+}
+
+$("#build").click(function build(){
+    var i;
+    $("#div1").empty();
+    for(i = 0; i < $("#divNum").val(); i++){
+        $("#div1").append("<div class='col-xs-6 col-lg-6'><img src='https://dodopizzaru-a.akamaihd.net/Img/Products/Pizza/c581dcc8-15e7-4a0b-aea2-45bae256c4ec.jpg'><p></p><div class='input-group'><div class='input-group-btn'><button type='button' id='plus' class='btn btn-default' onclick='add(" + i + ")'><span class='glyphicon glyphicon-plus'></span></button><button type='button' id='minus' class='btn btn-default' onclick='remove(" + i + ")'><span class='glyphicon glyphicon-minus'></span></button></div><div class='col-xs-3'><input type='text' value='0' id='" + "res" + i + "' " + "class='form-control' disabled></div></div></div><!--/.col-xs-6.col-lg-6-->");
     }
 })
