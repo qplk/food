3e433c71623875ad87698ae8c0d2da4f786288d4
qplk <Nail.khairullin@yandex.ru>
2017-04-12 15:16:37 +0300

    Update addition.js

--- src/main/webapp/resources/js/addition.js
+++ src/main/webapp/resources/js/addition.js
@@ -1,9 +1,3 @@
-$.ajax({
-type: 'POST',
-url: 'https://www.google.com/recaptcha/api/siteverify',
-
-})
-
 function add(i){
      var val = $("#res" + i).val();
      val++;
@@ -19,10 +13,30 @@ function remove(i){
     }
 }
 
-$("#build").click(function build(){
+function setCity(cityId){
+    $("#city").val(cityId);
+}
+
+function buildDrink(){
     var i;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/assortment' + '?restaurantId=' + $("#city").val() + "&category=" + $("#category1").val(), false);
+    xhr.send();
+    var data = JSON.parse(xhr.responseText);
     $("#div1").empty();
-    for(i = 0; i < $("#divNum").val(); i++){
-        $("#div1").append("<div class='col-xs-6 col-lg-6'><img src='https://dodopizzaru-a.akamaihd.net/Img/Products/Pizza/c581dcc8-15e7-4a0b-aea2-45bae256c4ec.jpg'><p></p><div class='input-group'><div class='input-group-btn'><button type='button' id='plus' class='btn btn-default' onclick='add(" + i + ")'><span class='glyphicon glyphicon-plus'></span></button><button type='button' id='minus' class='btn btn-default' onclick='remove(" + i + ")'><span class='glyphicon glyphicon-minus'></span></button></div><div class='col-xs-3'><input type='text' value='0' id='" + "res" + i + "' " + "class='form-control' disabled></div></div></div><!--/.col-xs-6.col-lg-6-->");
+    for(i = 0; i < data.length; i++){
+        $("#div1").append("<div class='col-sm-6 col-md-4'><div class='thumbnail'><img src='http://s020.radikal.ru/i709/1704/1a/79606a4bc27b.jpg'><div class='caption'><h4>" + data[i].foodName + "</h4><p>" + data[i].description + "</p><div class='input-group'><div class='input-group-btn'><button type='button' id='plus' class='btn btn-default' onclick='add(" + i + ")'><span class='glyphicon glyphicon-plus'></span></button><button type='button' id='minus' class='btn btn-default' onclick='remove(" + i + ")'><span class='glyphicon glyphicon-minus'></span></button></div><div class='col-xs-3'><input type='text' value='0' id='" + "res" + i + "' " + "class='form-control' disabled></div></div></div></div></div>");
     }
-})
+}
+
+function buildSnack(){
+    var i;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/assortment' + '?restaurantId=' + $("#city").val() + "&category=" + $("#category2").val(), false);
+    xhr.send();
+    var data = JSON.parse(xhr.responseText);
+    $("#div1").empty();
+    for(i = 0; i < data.length; i++){
+        $("#div1").append("<div class='col-sm-6 col-md-4'><div class='thumbnail'><img src='http://s020.radikal.ru/i709/1704/1a/79606a4bc27b.jpg'><div class='caption'><h4>" + data[i].foodName + "</h4><p>" + data[i].description + "</p><div class='input-group'><div class='input-group-btn'><button type='button' id='plus' class='btn btn-default' onclick='add(" + i + ")'><span class='glyphicon glyphicon-plus'></span></button><button type='button' id='minus' class='btn btn-default' onclick='remove(" + i + ")'><span class='glyphicon glyphicon-minus'></span></button></div><div class='col-xs-3'><input type='text' value='0' id='" + "res" + i + "' " + "class='form-control' disabled></div></div></div></div></div>");
+    }
+}
